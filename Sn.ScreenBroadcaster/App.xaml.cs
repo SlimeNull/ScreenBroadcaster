9ef6b3e720c2db4b83e3352c0cb3dda8de147ee8
SlimeNull <slimenull@outlook.com>
2024-06-19 10:41:36 +0800

    允许通过传命令行参数打开控制台

--- Sn.ScreenBroadcaster/App.xaml.cs
+++ Sn.ScreenBroadcaster/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Windows.Win32;
 
 namespace Sn.ScreenBroadcaster;
 
@@ -9,5 +10,14 @@ namespace Sn.ScreenBroadcaster;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        if (e.Args.Any(str => str.Equals("--console", StringComparison.OrdinalIgnoreCase)))
+        {
+            PInvoke.AllocConsole();
+        }
+
+        base.OnStartup(e);
+    }
 }
 
